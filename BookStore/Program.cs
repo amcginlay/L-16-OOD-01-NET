8ad201892d0c4205c749dd7e47bbed0b2428c0d3
amcginlay <amcginlay@gmail.com>
2016-02-10 16:04:53 +0000

    added program code

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -10,6 +10,30 @@ namespace BookStore
     {
         static void Main(string[] args)
         {
+            var basket = new Basket();
+            var checkout = new Checkout();
+
+            // go shopping
+            while (true)
+            {
+                Console.Write("Which Book?: ");
+                string bookTitle = Console.ReadLine();
+                Console.Write("Price?: ");
+                string bookPrice = Console.ReadLine();
+                var book = new Book() { Title = bookTitle.Trim(), Price = Double.Parse(bookPrice) };
+                basket.Add(book);
+                Console.Write("Any more? (y/n)");
+                string yesno = Console.ReadLine();
+                if (yesno.ToLower().StartsWith("n"))
+                {
+                    break;
+                }
+            }
+
+            // checkout
+            double total = checkout.Process(basket);
+            Console.WriteLine("The total is " + total);
+            Console.WriteLine("Have a nice day!");
         }
     }
 }
