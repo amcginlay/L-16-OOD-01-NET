a675b9e697d82440f3a351a30726381986da4cd0
amcginlay <amcginlay@gmail.com>
2016-02-10 11:12:33 +0000

    fixed

---
+++ FourPillarsAbstraction/MicrosoftDatabaseConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourPillarsAbstraction
+{
+    class MicrosoftDatabaseConnection : DatabaseConnection
+    {
+        public override void ConnectToDatabase(string url)
+        {
+            Console.WriteLine("Opening connection to Microsoft DB");
+        }
+    }
+}
---
+++ FourPillarsAbstraction/OracleDatabaseConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourPillarsAbstraction
+{
+    class OracleDatabaseConnection : DatabaseConnection
+    {
+        public override void ConnectToDatabase(string url)
+        {
+            Console.WriteLine("Opening connection to Oracle DB");
+        }
+    }
+}
