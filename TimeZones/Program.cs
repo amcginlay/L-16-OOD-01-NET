85595956737463c6a192f8df8dc67c8a68d12a6a
amcginlay <amcginlay@gmail.com>
2016-02-11 15:02:55 +0000

    Finished off console app code

--- TimeZones/Program.cs
+++ TimeZones/Program.cs
@@ -10,7 +10,24 @@ namespace TimeZones
     {
         static void Main(string[] args)
         {
-            //Console.ReadLine();
+            var utcTimeService = new UTCTimeService();
+            var timeResolver = new TimeResolver(utcTimeService);
+
+            while (true)
+            {
+                Console.Write("Enter city name: ");
+                var cityString = Console.ReadLine().Trim();
+                if (Enum.IsDefined(typeof(CityEnum), cityString))
+                {
+                    var city = (CityEnum)Enum.Parse(typeof(CityEnum), cityString);
+                    var resolvedTime = timeResolver.GetTime(city);
+                    Console.WriteLine("The time in {0} is {1}", city, resolvedTime);
+                }
+                else
+                {
+                    Console.WriteLine("Unrecognised, try again");
+                }
+            }
         }
     }
 }
