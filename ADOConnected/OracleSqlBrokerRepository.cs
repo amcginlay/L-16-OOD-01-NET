90ce5c42419e17c78e4e9bb451816eb4b9520a43
amcginlay <amcginlay@gmail.com>
2016-03-01 12:38:49 +0000

    ADOConnected

---
+++ ADOConnected/OracleSqlBrokerRepository.cs
@@ -0,0 +1,61 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOConnected
+{
+    public class OracleSqlBrokerRepository : IBrokerRepository
+    {
+        private string connectionString;
+        private List<Broker> allBrokers;
+
+        public OracleSqlBrokerRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+            allBrokers = new List<Broker>();
+        }
+
+        public void Refresh()
+        {
+            OracleConnection connection = null;
+            try
+            {
+                // Connection
+                connection = new OracleConnection(connectionString);
+                connection.Open();
+                // Command
+                string commandText = "SELECT broker_id, first_name, last_name FROM brokers";
+                OracleCommand command = new OracleCommand(commandText, connection);
+                // Reader
+                OracleDataReader dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    Broker broker = new Broker()
+                    {
+                        id = int.Parse(dataReader["broker_id"].ToString()),
+                        firstName = dataReader["first_name"].ToString(),
+                        lastName = dataReader["last_name"].ToString()
+                    };
+                    allBrokers.Add(broker);
+                }
+            }
+            //catch (OracleException exception)
+            //{
+
+            //}
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public List<Broker> GetAllBrokers()
+        {
+            return allBrokers;
+        }
+
+    }
+}
---
+++ ADOConnected/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOConnected
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ConnectionString
+            string connectionString = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=oracle.fdmgroup.com)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=xe)));Pooling=false;User Id=alanmcginlay;Password=Oracle101;";
+            IBrokerRepository brokerRepository = new OracleSqlBrokerRepository(connectionString);
+            brokerRepository.Refresh();
+            foreach(Broker broker in brokerRepository.GetAllBrokers())
+            {
+                Console.WriteLine("{0},{1},{2}", 
+                    broker.id, 
+                    broker.firstName, 
+                    broker.lastName);
+            }
+        }
+    }
+}
