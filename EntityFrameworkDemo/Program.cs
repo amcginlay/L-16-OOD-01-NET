5a640eede5554f801627aba0dfc367e81a32a334
amcginlay <amcginlay@gmail.com>
2016-03-01 17:02:57 +0000

    Added EntityFrameworkDemo

---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // add Matt (Matt does not exist at this point)
+            using (var context = new tradingsystemEntities())
+            {
+                broker b = new broker() { id = 4, name = "Matt" };
+                context.brokers.Add(b);
+                context.SaveChanges();
+            }
+
+            // list brokers
+            using(var context = new tradingsystemEntities())
+            {
+                foreach (var broker in context.brokers)
+                {
+                    Console.WriteLine(broker.name);
+                }
+            }
+
+            // delete Matt
+            using (var context = new tradingsystemEntities())
+            {
+                var brokers = context.brokers.Where(b => b.name == "Matt");
+                foreach (broker b in brokers)
+                {
+                    context.brokers.Remove(b);
+                }
+                context.SaveChanges();
+            }
+
+            // relations
+            Console.WriteLine("relations");
+            using (var context = new tradingsystemEntities())
+            {
+                var query = (from b in context.brokers
+                             where b.company.city == "Redmond"
+                             select b); 
+
+                foreach (var broker in query)
+                {
+                    Console.WriteLine(broker.name);
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ EntityFrameworkDemo/Setup.sql
@@ -0,0 +1,21 @@
+﻿IF OBJECT_ID('dbo.broker', 'U') IS NOT NULL DROP TABLE broker;
+IF OBJECT_ID('dbo.company', 'U') IS NOT NULL DROP TABLE company;
+
+CREATE TABLE company
+(
+      id INT PRIMARY KEY,
+      name VARCHAR(255),
+      city VARCHAR(255)
+);
+
+CREATE TABLE broker
+(
+      id INT PRIMARY KEY,
+      name VARCHAR(255),
+      fk_company_id INT FOREIGN KEY REFERENCES company(id),
+      city VARCHAR(255)
+      --,active INT
+);
+
+INSERT INTO company values (1, 'Microsoft', 'Redmond');
+INSERT INTO broker values (1, 'Dan', 1, 'London'); -- , 1);
